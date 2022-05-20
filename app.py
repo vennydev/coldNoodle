547e7517b06c6c49695c201296fc0b522949115b
HyeongseopLee <hs421@naver.com>
2022-05-20 15:26:11 +0900

    메인페이지 첫 화면 세팅

--- app.py
+++ app.py
@@ -13,13 +13,9 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
-
-# client = MongoClient('3.34.137.179', 27017, username="test",
-#                      password="sparta", tlsCAFile=certifi.where())
-# db = client.dbsparta_plus_week4
 client = MongoClient(
     'mongodb+srv://test:sparta@cluster0.l6xez.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-db = client.dbsparta
+db = client.dbsparta_plus_week4
 
 
 @app.route('/')
@@ -28,7 +24,10 @@ def home():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-        return render_template('index.html')
+    # 메인화면에 들어오면
+        user_info = db.users.find_one({"username": payload["username"]})
+        return render_template('index.html', user_info=user_info)
+
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -48,9 +47,10 @@ def user(username):
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-        status = (username == payload["id"])
+        status = (username == payload["username"])
 
-        user_info = db.users.find_one({"username": username}, {"_id": False})
+        user_info = db.user_info.find_one(
+            {"username": username}, {"username": False})
         return render_template('user.html', user_info=user_info, status=status)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
@@ -62,21 +62,29 @@ def sign_in():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
+    #  회원가입 때와 같은 방법으로 pw를 암호화합니다
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one(
-        {'id': username_receive, 'password': pw_hash})
-    print(result)
+
+    # username, 암호화된 pw를 가지고 해당 유저를 찾습니다
+    result = db.user_info.find_one(
+        {'username': username_receive, 'password': pw_hash})
+
+    #  해당 유저가 발견되면 jwt토큰을 만들어 발급합니다
     if result is not None:
-        # 매치되는 username,pw를 찾으면
+
+        #  jwt토큰에는 payload와 시크릿키가 필요합니다
+        #  시크릿키가 있어야 디코딩해서 payload 값을 볼 수 있습니다
+        # payload에는 username과 exp 정보가 들어있습니다
+        # 암호화된 jwt토큰을 유저에게 전달해줌
         payload = {
-            'id': username_receive,
+            'username': username_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        # 시크릿키로 암호화를 해준 뒤 jwt토큰에 넣는다
         token = jwt.encode(payload, SECRET_KEY,
                            algorithm='HS256')
         return jsonify({'result': 'success', 'token': token})
-    # 매치되는 id,pw를 찾지 못 하면
+
+    # 매치되는 username,pw를 찾지 못 하면 실패 메시지를 보냅니다
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
@@ -90,18 +98,21 @@ def sign_up():
         password_receive.encode('utf-8')).hexdigest()
     # DB에 저장
     doc = {
-        'id': username_receive,
+        'username': username_receive,
         'password': password_hash,
         'profile_name': username_receive,
+        "profile_pic": "",
+        "profile_pic_real": "profile_pics/profile_placeholder.png",
+        "profile_info": "",
     }
-    db.users.insert_one(doc)
+    db.user_info.insert_one(doc)
     return jsonify({'result': '후루룩후루룩'})
 
 
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"username": username_receive}))
+    exists = bool(db.user_info.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
 
